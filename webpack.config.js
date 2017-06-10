1e9ff1e90d4a8c201616c86255763847b8f659ad
draviteja <raviteja@vasista.in>
2017-06-10 17:38:07 +0530

    htmlwebpack template location

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var HTMLWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-  template: __dirname + '/build/index.html',
+  template: __dirname + '/app/index.html',
   filename: 'index.html',
   inject: 'body'
 });
