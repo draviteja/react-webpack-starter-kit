997784e38e908f87036183e8bd069cab71ae1f19
Raviteja Daggupati <raviteja5255@gmail.com>
2019-03-29 19:36:59 +0530

    added comment in entry file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './style.css';
 import HelloWorld from './components/Hello';
 
+//this is entry point
 ReactDOM.render(
   <HelloWorld />, document.getElementById('app')
 );
